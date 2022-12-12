ed0ec42add016b9826208b0bf2e2641f5abbc083
logan-larson <loganlarson20@gmail.com>
2022-12-12 10:12:33 -0600

    I did a lot

--- Assets/Scripts/PlayerComponents/PlayerVelocity.cs
+++ Assets/Scripts/PlayerComponents/PlayerVelocity.cs
@@ -5,5 +5,4 @@ public class PlayerVelocity : MonoBehaviour
     public float x;
     public float y;
     public Vector2 veloOffGround;
-    public float angleOffGround;
 }
--- Assets/Scripts/PlayerSystems/MovementSystem.cs
+++ Assets/Scripts/PlayerSystems/MovementSystem.cs
@@ -7,20 +7,71 @@ using UnityEngine;
 [RequireComponent (typeof(PlayerMovementProperties))]
 [RequireComponent (typeof(PlayerGrounded))]
 [RequireComponent (typeof(PlayerMode))]
-public class MovementSystem : NetworkBehaviour {
+public class MovementSystem : NetworkBehaviour
+{
+
+    #region Types
+
+    /// <summary>
+    /// All the data needed to move the player on the client and server.
+    /// </summary>
+    public struct MoveData
+    {
+        public float Horizontal;
+        public bool Jump;
+        public bool Sprint;
+        public bool ModeChange; // Sprint key or fire key
+    }
+
+    /// <summary>
+    /// All the data needed to reconcile the player on the client and server.
+    /// </summary>
+    public struct ReconcileData
+    {
+        public Vector2 Position;
+        public float Rotation;
+        public Vector2 Velocity;
+        public Vector2 AirborneVelocity;
+        public bool IsGrounded;
+        public bool InParkourMode;
+        public bool InCombatMode;
+    }
+
+    #endregion
+
+    #region Script References
 
     PlayerInput input;
-    PlayerPosition position;
-    PlayerVelocity velocity;
     PlayerMovementProperties movementProperties;
-    PlayerGrounded grounded;
-    PlayerMode mode;
+
+    #endregion
+
+    #region Public Variables
+
+    public Vector2 Position; 
+    public float Rotation;
+    public Vector2 Velocity;
+    public Vector2 AirborneVelocity;
+
+    public bool IsGrounded;
+    public float GroundDistance;
+    public LayerMask Mask;
+    public bool InParkourMode;
+    public bool InCombatMode;
+
 
     RaycastOrigins raycastOrigins;
 
     public struct RaycastOrigins {
         public Vector2 topLeft, topRight, bottomLeft, bottomRight;
     }
+
+    #endregion
+
+    #region Private Variables
+
+
+
 
     bool jumpNow = false;
 
@@ -31,13 +82,16 @@ public class MovementSystem : NetworkBehaviour {
     public float fFactor = 6.75f;
     public float minJumpTime = 1f;
 
+
+    #endregion
+
     private void Start() { // public void OnStart
         input = GetComponent<PlayerInput>();
-        position = GetComponent<PlayerPosition>();
-        velocity = GetComponent<PlayerVelocity>();
         movementProperties = GetComponent<PlayerMovementProperties>();
-        grounded = GetComponent<PlayerGrounded>();
-        mode = GetComponent<PlayerMode>();
+    }
+
+    private void Update() {
+
     }
 
     private void FixedUpdate() { // public void OnUpdate
@@ -59,10 +113,10 @@ public class MovementSystem : NetworkBehaviour {
             
 
         // This is a velocity relative to the player not the world
-        Vector2 adjVelo = new Vector2(velocity.x, velocity.y) * Time.fixedDeltaTime;
+        Vector2 adjVelo = new Vector2(Velocity.x, Velocity.y) * Time.fixedDeltaTime;
 
         // If grounded, match body to ground
-        if (grounded.isGrounded) {
+        if (IsGrounded) {
             //movementProperties.maxRotationDegrees = 2f;
 
             ShowPredictedTrajectory();
@@ -70,14 +124,14 @@ public class MovementSystem : NetworkBehaviour {
             Ray2D leftRay = new Ray2D(raycastOrigins.bottomLeft + adjVelo, -transform.up);
             Ray2D rightRay = new Ray2D(raycastOrigins.bottomRight + adjVelo, -transform.up);
 
-            RaycastHit2D leftHit = Physics2D.Raycast(leftRay.origin, leftRay.direction, movementProperties.groundedHeight, grounded.mask);
-            RaycastHit2D rightHit = Physics2D.Raycast(rightRay.origin, rightRay.direction, movementProperties.groundedHeight, grounded.mask);
+            RaycastHit2D leftHit = Physics2D.Raycast(leftRay.origin, leftRay.direction, movementProperties.groundedHeight, Mask);
+            RaycastHit2D rightHit = Physics2D.Raycast(rightRay.origin, rightRay.direction, movementProperties.groundedHeight, Mask);
 
             // Use override hit to prevent clipping
-            RaycastHit2D overrideHit = Physics2D.Raycast((leftRay.origin + rightRay.origin) / 2, transform.right, movementProperties.overrideRayLength * Mathf.Sign(velocity.x), grounded.mask);
-            if (velocity.x < 0f && overrideHit.collider != null) {
+            RaycastHit2D overrideHit = Physics2D.Raycast((leftRay.origin + rightRay.origin) / 2, transform.right, movementProperties.overrideRayLength * Mathf.Sign(Velocity.x), Mask);
+            if (Velocity.x < 0f && overrideHit.collider != null) {
                 leftHit = overrideHit;
-            } else if (velocity.x > 0f && overrideHit.collider != null) {
+            } else if (Velocity.x > 0f && overrideHit.collider != null) {
                 rightHit = overrideHit;
             }
 
@@ -90,33 +144,33 @@ public class MovementSystem : NetworkBehaviour {
                 Quaternion finalRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, movementProperties.maxRotationDegrees);
 
                 transform.rotation = Quaternion.Euler(0f, 0f, finalRotation.eulerAngles.z);
-                position.rotation = finalRotation.eulerAngles.z;
+                Rotation = finalRotation.eulerAngles.z;
             }
 
             transform.Translate(adjVelo);
-            position.x = transform.position.x;
-            position.y = transform.position.y;
+            Position.x = transform.position.x;
+            Position.y = transform.position.y;
         } else { 
             //movementProperties.maxRotationDegrees = 8f;
             // We are in the air, so move according to worldspace not self
-            transform.position += new Vector3(velocity.veloOffGround.x, velocity.veloOffGround.y, 0f) * Time.fixedDeltaTime;
+            transform.position += new Vector3(AirborneVelocity.x, AirborneVelocity.y, 0f) * Time.fixedDeltaTime;
 
-            position.x = transform.position.x;
-            position.y = transform.position.y;
+            Position.x = transform.position.x;
+            Position.y = transform.position.y;
 
             // And rotate to the predicted landing spots normal
             Quaternion targetRotation = Quaternion.FromToRotation(Vector3.up, predictNorm);
             Quaternion finalRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, movementProperties.maxRotationDegrees);
 
             transform.rotation = Quaternion.Euler(0f, 0f, finalRotation.eulerAngles.z);
-            position.rotation = finalRotation.eulerAngles.z;
+            Rotation = finalRotation.eulerAngles.z;
         }
     }
 
     void UpdateMode() {
         if (input.isSprintKeyPressed) {
-            mode.inCombatMode = false;
-            mode.inParkourMode = true;
+            InCombatMode = false;
+            InParkourMode = true;
         }
     }
 
@@ -128,30 +182,30 @@ public class MovementSystem : NetworkBehaviour {
     }
 
     void UpdateGrounded() {
-        if (grounded.isGrounded) {
-            RaycastHit2D groundedHit = Physics2D.Raycast(transform.position, -transform.up, movementProperties.groundedHeight * 1.5f, grounded.mask);
-            grounded.isGrounded = groundedHit.collider != null;
-            grounded.groundDistance = groundedHit.distance;
+        if (IsGrounded) {
+            RaycastHit2D groundedHit = Physics2D.Raycast(transform.position, -transform.up, movementProperties.groundedHeight * 1.5f, Mask);
+            IsGrounded = groundedHit.collider != null;
+            GroundDistance = groundedHit.distance;
         } else {
-            RaycastHit2D groundedHit = Physics2D.Raycast(transform.position, -transform.up, movementProperties.groundedHeight, grounded.mask);
-            grounded.isGrounded = groundedHit.collider != null;
-            grounded.groundDistance = groundedHit.distance;
+            RaycastHit2D groundedHit = Physics2D.Raycast(transform.position, -transform.up, movementProperties.groundedHeight, Mask);
+            IsGrounded = groundedHit.collider != null;
+            GroundDistance = groundedHit.distance;
         }
     }
 
     public void Jump() {
-        if (grounded.isGrounded) {
+        if (IsGrounded) {
             jumpNow = true;
         }
     }
 
     void UpdateVelocity() {
         // Add velo based on horizontal input, accelerate faster when sprint key is pressed
-        if (grounded.isGrounded) {
+        if (IsGrounded) {
             if (input.isSprintKeyPressed) {
-                velocity.x += input.horizontalMovementInput * (movementProperties.acceleration * 2f);
+                Velocity.x += input.horizontalMovementInput * (movementProperties.acceleration * 2f);
             } else {
-                velocity.x += input.horizontalMovementInput * movementProperties.acceleration;
+                Velocity.x += input.horizontalMovementInput * movementProperties.acceleration;
             }
 
             predictPos = new Vector3();
@@ -159,39 +213,39 @@ public class MovementSystem : NetworkBehaviour {
         }
 
         // Apply friction when no input is held
-        if (grounded.isGrounded && input.horizontalMovementInput == 0f) {
-            if (Mathf.Abs(velocity.x - movementProperties.friction) < 0.5f) {
-                velocity.x = 0f;
+        if (IsGrounded && input.horizontalMovementInput == 0f) {
+            if (Mathf.Abs(Velocity.x - movementProperties.friction) < 0.5f) {
+                Velocity.x = 0f;
             } else {
-                if (velocity.x > 0f) {
-                    velocity.x = Mathf.Clamp(velocity.x, 0f, velocity.x - movementProperties.friction);
+                if (Velocity.x > 0f) {
+                    Velocity.x = Mathf.Clamp(Velocity.x, 0f, Velocity.x - movementProperties.friction);
                 } else {
-                    velocity.x = Mathf.Clamp(velocity.x, velocity.x + movementProperties.friction, 0f);
+                    Velocity.x = Mathf.Clamp(Velocity.x, Velocity.x + movementProperties.friction, 0f);
                 }
             }
         }
 
         // Set height above ground
-        if (grounded.isGrounded) {
+        if (IsGrounded) {
             if (jumpNow) {
                 jumpNow = false;
-                grounded.isGrounded = false;
+                IsGrounded = false;
                 movementProperties.timeSinceGrounded = 0f;
-                velocity.y = movementProperties.jumpVelocity;
+                Velocity.y = movementProperties.jumpVelocity;
             } else { // This needs a lot of work to make player landings smoother
-                if (grounded.groundDistance < 0.9f) {
-                    velocity.y += 0.05f; 
-                } else if (grounded.groundDistance > 1f) {
-                    velocity.y -= 0.05f; 
+                if (GroundDistance < 0.9f) {
+                    Velocity.y += 0.05f; 
+                } else if (GroundDistance > 1f) {
+                    Velocity.y -= 0.05f; 
                 } else {
-                    velocity.y = 0f;
+                    Velocity.y = 0f;
                 }
             }
 
-            velocity.veloOffGround = new Vector2(0f, 0f);
+            AirborneVelocity = new Vector2(0f, 0f);
         } else {
             // Add gravity in downward direction relative to worldspace
-            velocity.veloOffGround += (Vector2.down * movementProperties.gravity * Time.fixedDeltaTime);
+            AirborneVelocity += (Vector2.down * movementProperties.gravity * Time.fixedDeltaTime);
 
             // This is where airborne movement forces can be applied
 
@@ -200,7 +254,7 @@ public class MovementSystem : NetworkBehaviour {
         }
 
         // Set sprint multiplier
-        if (input.isSprintKeyPressed && mode.inParkourMode) {
+        if (input.isSprintKeyPressed && InParkourMode) {
             movementProperties.sprintMultiplier += movementProperties.acceleration * 2f;
             movementProperties.sprintMultiplier = Mathf.Clamp(movementProperties.sprintMultiplier, 0f, movementProperties.maxSprintSpeed);
         } else {
@@ -209,10 +263,10 @@ public class MovementSystem : NetworkBehaviour {
         }
 
         // Limit top speed
-        velocity.x = Mathf.Clamp(velocity.x, -movementProperties.maxXSpeed - movementProperties.sprintMultiplier, movementProperties.maxXSpeed + movementProperties.sprintMultiplier);
+        Velocity.x = Mathf.Clamp(Velocity.x, -movementProperties.maxXSpeed - movementProperties.sprintMultiplier, movementProperties.maxXSpeed + movementProperties.sprintMultiplier);
 
         // This is triggered directly after the jump is initialized
-        if (!grounded.isGrounded && movementProperties.timeSinceGrounded == 0f) {
+        if (!IsGrounded && movementProperties.timeSinceGrounded == 0f) {
             // Get the velocity directly after the jump because we don't allow
             // airborne movement controls besides using a weapon to move
 
@@ -229,22 +283,22 @@ public class MovementSystem : NetworkBehaviour {
             float theta = Vector2.SignedAngle(Vector2.right, transform.up) * Mathf.Deg2Rad;
             float gamma = Vector2.SignedAngle(Vector2.right, transform.right) * Mathf.Deg2Rad;
 
-            float xComponentOfYVelo = Mathf.Cos(theta) * velocity.y;
-            float yComponentOfYVelo = Mathf.Sin(theta) * velocity.y;
+            float xComponentOfYVelo = Mathf.Cos(theta) * Velocity.y;
+            float yComponentOfYVelo = Mathf.Sin(theta) * Velocity.y;
 
-            float xComponentOfXVelo = Mathf.Cos(gamma) * velocity.x;
-            float yComponentOfXVelo = Mathf.Sin(gamma) * velocity.x;
+            float xComponentOfXVelo = Mathf.Cos(gamma) * Velocity.x;
+            float yComponentOfXVelo = Mathf.Sin(gamma) * Velocity.x;
 
-            velocity.veloOffGround = new Vector2(xComponentOfXVelo + xComponentOfYVelo, yComponentOfXVelo + yComponentOfYVelo);
+            AirborneVelocity = new Vector2(xComponentOfXVelo + xComponentOfYVelo, yComponentOfXVelo + yComponentOfYVelo);
 
             // Reset ground velocities
-            velocity.x = 0f;
-            velocity.y = 0f;
+            Velocity.x = 0f;
+            Velocity.y = 0f;
 
-            Debug.Log("Initial velo off ground: (" + velocity.veloOffGround.x + ", " + velocity.veloOffGround.y + ")");
+            Debug.Log("Initial velo off ground: (" + AirborneVelocity.x + ", " + AirborneVelocity.y + ")");
 
             RecalculateLandingPos();
-        } else if (!grounded.isGrounded && predictNorm == new Vector3() && predictPos == new Vector3()) {
+        } else if (!IsGrounded && predictNorm == new Vector3() && predictPos == new Vector3()) {
             // Ideally this never gets called because the player should never detach
             // from a surface unless they jump
             RecalculateLandingPos();
@@ -260,7 +314,7 @@ public class MovementSystem : NetworkBehaviour {
             Vector2 pos = transform.position - (transform.up * 0.7f);
             Vector2 pos2 = transform.position + (transform.up * 0.7f);
 
-            Vector2 velo = new Vector2(velocity.veloOffGround.x, velocity.veloOffGround.y) * Time.fixedDeltaTime * fFactor;
+            Vector2 velo = new Vector2(AirborneVelocity.x, AirborneVelocity.y) * Time.fixedDeltaTime * fFactor;
 
             int count = 0;
             while ((predictHit.collider == null && predictHit2.collider == null) && count < 100) {
@@ -275,8 +329,8 @@ public class MovementSystem : NetworkBehaviour {
                 Debug.DrawRay(ray2.origin, ray2.direction * velo.magnitude, randColor, 2f);
 
                 // Update predictHit
-                predictHit = Physics2D.Raycast(ray.origin, ray.direction, velo.magnitude, grounded.mask);
-                predictHit2 = Physics2D.Raycast(ray2.origin, ray2.direction, velo.magnitude, grounded.mask);
+                predictHit = Physics2D.Raycast(ray.origin, ray.direction, velo.magnitude, Mask);
+                predictHit2 = Physics2D.Raycast(ray2.origin, ray2.direction, velo.magnitude, Mask);
 
                 // Update position to end of predictHit ray
                 pos += (ray.direction * velo.magnitude);
@@ -317,8 +371,8 @@ public class MovementSystem : NetworkBehaviour {
         float xComponentOfYVelo = Mathf.Cos(theta) * movementProperties.jumpVelocity;
         float yComponentOfYVelo = Mathf.Sin(theta) * movementProperties.jumpVelocity;
 
-        float xComponentOfXVelo = Mathf.Cos(gamma) * velocity.x;
-        float yComponentOfXVelo = Mathf.Sin(gamma) * velocity.x;
+        float xComponentOfXVelo = Mathf.Cos(gamma) * Velocity.x;
+        float yComponentOfXVelo = Mathf.Sin(gamma) * Velocity.x;
 
         Vector2 predVelo = new Vector2(xComponentOfXVelo + xComponentOfYVelo, yComponentOfXVelo + yComponentOfYVelo);
 
@@ -338,7 +392,7 @@ public class MovementSystem : NetworkBehaviour {
             }
 
             // Update predictHit
-            predictHit = Physics2D.Raycast(ray.origin, ray.direction, velo.magnitude, grounded.mask);
+            predictHit = Physics2D.Raycast(ray.origin, ray.direction, velo.magnitude, Mask);
 
             // Update position to end of predictHit ray
             pos += (ray.direction * velo.magnitude);
