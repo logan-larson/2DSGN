cf001542dc52d42b55a4e155f4e7b0fe2101f747
Logan Larson <61062214+logan-larson@users.noreply.github.com>
2023-04-15 11:51:26 -0500

    Refactored Camera scripts

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 
-public class CameraTarget : MonoBehaviour
+/// <summary>
+/// Class <c>CameraController</c> moves the camera's position and rotation to follow the player's position and rotation.
+/// The script waits for the player to spawn before it can start following the player.
+/// The camera's position and rotation is lerped to the player's position and rotation.
+/// </summary>
+public class CameraController : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private Transform player = null;
@@ -8,7 +13,10 @@ public class CameraTarget : MonoBehaviour
     [SerializeField] private float rotLerpValue = 0.05f;
 
     [SerializeField] private float startingZ = -10f;
-    [SerializeField] private float zMod = 0.4f;
+    /// <summary>
+    /// The factor by which the z position is effected by the player's velocity.
+    /// </summary>
+    [SerializeField] private float zFactor = 0.4f;
 
     private Vector3 velocity = Vector3.zero;
 
@@ -38,37 +46,20 @@ public class CameraTarget : MonoBehaviour
     {
         if (player == null) return;
 
-        // Get the mouse position in world space
-        Vector3 mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
-
-        // Set the target position to the player position
-        Vector3 targetPos = player.position;
-
-
         // Calculate the camera z position based on the player's velocity
         // Zoom out when moving faster
         Vector3 velocity = movementSystem.PublicData.Velocity;
 
-        targetPos.z = startingZ - (velocity.magnitude * zMod);
+        Vector3 targetPos = player.position;
+
+        targetPos.z = startingZ - (velocity.magnitude * zFactor);
 
         targetPos.z = Mathf.Clamp(targetPos.z, -100f, -1f);
 
-        // Round target position to nearest pixel
-        Vector3 vectorInPixels = new Vector3(
-            Mathf.RoundToInt(targetPos.x * 32),
-            Mathf.RoundToInt(targetPos.y * 32),
-            Mathf.RoundToInt(targetPos.z * 32)
-        );
-
-        // Convert position to units
-        Vector3 posInUnits = vectorInPixels / 32;
-
         // Lerp to nearest position and rotation
-        Vector3 lerpedPos = Vector3.Lerp(transform.position, posInUnits, posLerpValue);
-        // Vector3 dampedPos = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, posLerpValue);
+        Vector3 lerpedPos = Vector3.Lerp(this.transform.position, targetPos, posLerpValue);
         Quaternion lerpedRot = Quaternion.Lerp(this.transform.rotation, player.rotation, rotLerpValue);
 
         this.transform.SetPositionAndRotation(new Vector3(lerpedPos.x, lerpedPos.y, lerpedPos.z), lerpedRot);
-        // this.transform.SetPositionAndRotation(new Vector3(dampedPos.x, dampedPos.y, dampedPos.z), lerpedRot);
     }
 }
